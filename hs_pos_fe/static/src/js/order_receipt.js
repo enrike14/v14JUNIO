b950c7c344049e11a98e5537a4f0160c401defff
enrike14 <evelasquez1485@gmail.com>
2022-07-01 10:08:21 -0500

    test  console log

--- hs_pos_fe/static/src/js/order_receipt.js
+++ hs_pos_fe/static/src/js/order_receipt.js
@@ -10,7 +10,20 @@ odoo.define("hs_pos_fe.order_receipt", function (require) {
         console.log("ENTRÖ:::::::::");
         super(...arguments);
 
-        this._receiptEnv = this.props.order.getOrderReceiptEnv();
+        // this._receiptEnv = this.props.order.getOrderReceiptEnv();
+      }
+      get fe_receipt_render() {
+        console.log("ENTRÖ222222:::::::::");
+        /*         return {
+          cufe: localStorage.getItem("cufe"),
+          qr_code: localStorage.getItem("qr_code"),
+          qr_img:
+            "/web/image?model=pos.order&id=" +
+            localStorage.getItem("id") +
+            "&field=qr_code",
+          is_pos: localStorage.getItem("is_pos"),
+        }; */
+        return true;
       }
     };
 
