9d8e59b3c0573a291e74282b8db14a7b19148fcc
enrike14 <evelasquez1485@gmail.com>
2022-07-15 13:49:47 -0500

    fix errors district

--- hs_electronic_invoice/models/__init__.py
+++ hs_electronic_invoice/models/__init__.py
@@ -7,7 +7,7 @@ from . import electronic_invoice_logs
 from . import product_field
 from . import product_variants_field
 from . import account_move_reversal
-#from . import customers_fields
+from . import customers_fields
 from . import country_fields
 from . import electronic_invoice_cpbs
 from . import electronic_invoice_province
